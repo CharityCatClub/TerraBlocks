41619f7c3c9d1b1aea1876739faf8bad6e2d26a6
Sam Hui <samuel.hk.ca@gmail.com>
2026-09-02 19:45:02 -0700

    V54 trying to resolve heat issue

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788395498|35505212';
+const CACHE_VERSION = '1788403404|36472659';
 /** @type {string} */
 const CACHE_PREFIX = 'TerraBlocks-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
